c5f1e9f4c5a21a812f630004857ff4ed66f1d1f6
AlfonsoDiaz09 <alfonsodiazc.09@gmail.com>
2024-02-27 15:02:48 -0600

    fix: Se corrigieron los nombres de las funciones y parametros Se realizó la corrección de los nombres de las funciones para que sean más entendibles sobre cuál es la acción que va a realizar, además, en una de las funciones que contaba con 4 parámetros se hizo una desestructuración creando una interfaz donde se indicaba el tipo de dato para cada uno y de esta forma hacerlo más legible, por último se creó una nueva función para delegarle la acción de verificar si el nombre completo era válido y de esta forma no hacer mucho código en la función de crear un Actor

--- src/03-functions.ts
+++ src/03-functions.ts
@@ -1,34 +1,46 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
+    function getMovieById( id: string ) {
+        console.log({ id });
     }
 
     // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    function getMovieCastById( id: string ) {
         console.log({ id });
     }
 
     // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    interface Movie{
+        cast: string[]
+        description: string, 
+        rating: number, 
+        title: string,
+    }
+    function getActorBioById( id: string ) {
+        console.log({ id });
     }
     
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
-        console.log({ title, description, rating, cast });
+    function createMovie({ cast, description, rating, title }: Movie ) {
+        console.log({ cast, description, rating, title });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+    function checkName( fullName: string ): boolean {
+        console.log({ fullName });
+        return true;
+    }
+
+
+    function createActor( fullName: string, birthDate: Date ): boolean {
         
         // tarea asincrona para verificar nombre
         // ..
         // ..
-        if ( fullName === 'fernando' ) return false;
+        if ( checkName( fullName ) ) return false;
 
-        console.log('Crear actor');
+        console.log('Crear actor', birthDate);
         return true;        
 
     }
