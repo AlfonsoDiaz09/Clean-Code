c1902d5cc4e65cfc34da4f75827b5770c8ed4929
AlfonsoDiaz09 <alfonsodiazc.09@gmail.com>
2024-02-27 18:27:39 -0600

    feat: Se agrega una nueva función de cantidad de pago Se crea una función nueva que permite obtener el valor de pago dependiendo el estado de la persona, si esta muerta, separada, retirada, pero cuenta con mucho código innecesario

--- src/03-functions.ts
+++ src/03-functions.ts
@@ -45,7 +45,26 @@
 
     }
 
-    
+    // Continuar
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+        let result: number;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
 
 
 })();
